8426026ba907efff51d5bf5822855369c73fe180
Frederic Branca <frederic.branca@gmail.com>
2023-05-12 13:42:40 +0200

    function open and  close modal

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,18 @@
+// Fonctions
+
+    // Fonction pour ouvrir le modal
+    function openModal() {
+        document.querySelector('.overlay').style.zIndex = '1';
+        document.querySelector('.overlay').style.opacity = '1';
+        document.querySelector('.modal').classList.add('modal-open');
+    }
+    // Fonction pour fermer le modal
+    function closeModal() {
+        document.querySelector('.overlay').style.opacity = '0';
+        document.querySelector('.overlay').style.zIndex = '-1';
+        document.querySelector('.modal').classList.remove('modal-open');
+    }
+
 const caroussel = document.getElementById('caroussel');
 const cards = caroussel.querySelector('.cards');
 
