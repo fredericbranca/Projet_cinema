07198d0c87f505647d25f87f9d1bc4f5e0202285
Frederic Branca <frederic.branca@gmail.com>
2023-05-15 20:59:31 +0200

    Display genre/director + add film & genre

--- controller/CinemaController.php
+++ controller/CinemaController.php
@@ -70,18 +70,71 @@ class CinemaController {
 
         $pdo = Connect::seConnecter();
 
-        // foreach($acteurs as $acteur) {
-
-        // }
-
         $requeteTitres = $pdo->query("
             SELECT id_film, titre
             FROM film
         ");
-        $requeteAddFilm = $pdo->prepare("
-            INSERT INTO 
+        $requeteGenre = $pdo->query("
+            SELECT *
+            FROM genre
+        ");
+        $requeteRealisateur = $pdo->query("
+            SELECT r.id_realisateur as id_realisateur, CONCAT(p.nom, ' ', p.prenom) as name
+            FROM personne p
+            JOIN realisateur r ON r.id_personne = p.id_personne
         ");
 
+        // Requete ajouter un film
+
+        if (isset($_POST['filmSubmit'])) {
+
+            //Filtre
+            $titre = filter_input(INPUT_POST, 'titre', FILTER_SANITIZE_FULL_SPECIAL_CHARS); // empèche injection de SQL ou de HTML, supprime toutes les balises
+            $dateSortie = filter_input(INPUT_POST, 'dateSortie', FILTER_SANITIZE_FULL_SPECIAL_CHARS); 
+            $duree = filter_input(INPUT_POST, 'duree', FILTER_VALIDATE_INT);
+            $synopsis = filter_input(INPUT_POST, 'synopsis', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $note = filter_input(INPUT_POST, 'note', FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION); //FILTER_VALIDATE_FLOAT (champ "price") : validera le prix que s'il est un nombre à virgule (pas de texte ou autre…), le drapeau FILTER_FLAG_ALLOW_FRACTION est ajouté pour permettre l'utilisation du caractère "," ou "." pour la décimale.
+            $affiche = filter_input(INPUT_POST, 'affiche', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $idRealisateur = filter_input(INPUT_POST, 'idRealisateur', FILTER_VALIDATE_INT);
+            $idGenres = filter_input(INPUT_POST, 'idGenre', FILTER_SANITIZE_NUMBER_INT, FILTER_REQUIRE_ARRAY); // FILTER_REQUIRE_ARRAY : on veut récupérer les valuers sous forme de tableau (idGenre[])
+
+            //check les filtres
+            if($titre !==false && $dateSortie !==false && $duree !==false && $synopsis !==false && $note !==false && $affiche !==false && $idRealisateur !==false && $idGenres !==false) {
+            
+            // Requete pour ajouter un film à la DB
+            $requeteAddFilm = $pdo->prepare("
+                INSERT INTO film (titre, dateSortie, duree, synopsis, note, affiche, id_realisateur)
+                VALUES (:titre, :dateSortie, :duree, :synopsis, :note, :affiche, :idRealisateur)
+            ");
+            // Requete pour ajouter un ou plusieurs genre au film ajouté
+            $requeteAddGenre = $pdo->prepare("
+                INSERT INTO genre_film (id_film, id_genre)
+                SELECT LAST_INSERT_ID(), :idGenre;
+            ");
+
+            $requeteAddFilm->execute([
+                                    'titre' => $titre,
+                                    'dateSortie' => $dateSortie,
+                                    'duree' => $duree,
+                                    'synopsis' => $synopsis,
+                                    'note' => $note,
+                                    'affiche' => $affiche,
+                                    'idRealisateur' => $idRealisateur
+            ]);
+            foreach($idGenres as $idGenre) {
+                                    $requeteAddGenre->execute([
+                                        'idGenre' => $idGenre
+                                    ]);
+            }
+
+            // Tentative de redirection sur le modal après l'ajout du film
+            // if ($requeteAddFilm) {
+                // header("Location: index.php?action=admin"); 
+                // exit; // à check l'utilité
+            // }
+
+            }
+        }
         require "view/admin.php";
     }
 }
