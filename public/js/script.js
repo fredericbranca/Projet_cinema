88a96abcdede3b6181aab5aa8f611da494cddc98
Frederic Branca <frederic.branca@gmail.com>
2023-05-15 21:07:00 +0200

    adding comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,7 @@
 // Fonctions
 
     // Fonction pour ouvrir le modal
+    // Ces fonctions modifient le style css du modal pour le rendre visible ou invisible
     function openModal() {
         document.querySelector('.overlay').style.zIndex = '1';
         document.querySelector('.overlay').style.opacity = '1';
@@ -13,20 +14,26 @@
         document.querySelector('.modal').classList.remove('modal-open');
     }
 
-const caroussel = document.getElementById('caroussel');
-const cards = caroussel.querySelector('.cards');
+// Caroussel, liste des films de la page d'accueil
 
-const chevronGauche = document.getElementById('chevron-gauche');
-const chevronDroit = document.getElementById('chevron-droit');
+    // On récupère les éléments HTML
+    const caroussel = document.getElementById('caroussel');
+    const cards = caroussel.querySelector('.cards');
 
-p = 0;
+    const chevronGauche = document.getElementById('chevron-gauche');
+    const chevronDroit = document.getElementById('chevron-droit');
+    
+    // Variable pour le déplacement du carrousel, elle détermine la position actuelle du carrousel
+    let p = 0;
 
-chevronGauche.onclick=function(){
-    p++
-    caroussel.style.transform="translate(" + p * 367 + "px)";
-}
-  
-chevronDroit.onclick=function(){
-    p--
-    caroussel.style.transform="translate(" + p * 367 + "px)";
-}
+    // Incrémente la valeur p et déplace le carrousel vers la droite
+    chevronGauche.onclick=function(){
+        p++
+        caroussel.style.transform="translate(" + p * 367 + "px)";
+    }
+    
+    //// Décrémente la valeur p et déplace le carrousel vers la gauche
+    chevronDroit.onclick=function(){
+        p--
+        caroussel.style.transform="translate(" + p * 367 + "px)";
+    }
