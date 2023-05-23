5d86fb9ee9d279edb351eec58a7195d424302fe7
Frederic Branca <frederic.branca@gmail.com>
2023-05-23 16:58:55 +0200

    add ActeurController and RealisateurController

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 session_start();
 
 use Controller\CinemaController; // On "use" le controller Cinema
+use Controller\ActeurController;
+use Controller\RealisateurController;
 
 // On autocharge les classes du projet
 spl_autoload_register(function ($class_name) {
@@ -10,14 +12,25 @@ spl_autoload_register(function ($class_name) {
 });
 
 $ctrlCinema = new CinemaController(); // On instancie le controller Cinema
+$ctrlActeur = new ActeurController();
+$ctrlRealisateur = new RealisateurController();
+
 
 $id = (isset($_GET["id"])) ? $_GET["id"] : null;
 
 if(isset($_GET["action"])) {
 
     switch ($_GET["action"]) {
+
+        // CinemaController
         case "accueil" : $ctrlCinema -> listFilms(); break;
         case "detailsFilm" : $ctrlCinema -> detailsFilm($id); break;
         case "admin" : $ctrlCinema -> addFilm(); break;
+
+        //ActeurController
+        case "detailsActeur" : $ctrlActeur -> listActeurs($id); break;
+
+        //RealisateurController
+        case "detailsRealisateur" : $ctrlRealisateur -> listRealisateurs($id); break;
     }
 }
