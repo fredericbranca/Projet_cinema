148ba512fe652f3618532fd3a8bb2893585332e3
Frederic Branca <frederic.branca@gmail.com>
2023-05-23 16:59:59 +0200

    add RealisateurController

---
+++ controller/RealisateurController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Controller;
+
+use Model\Connect; // "use" pour accéder à la classe Connect située dans le namespace "Model"
+
+
+class RealisateurController
+{
+
+    /* Lister les réalisateurs */
+    public function listRealisateurs($id)
+    {
+        $pdo = Connect::seConnecter();
+
+        $requeteLangue = $pdo->query("SET lc_time_names = 'fr_FR';");
+
+        // Requete pour afficher les info de l'acteur depuis l'id
+        $requeteAfficherActeur = $pdo->prepare("
+        SELECT CONCAT(p.nom, ' ', P.prenom) AS realisateur, sexe, DATE_FORMAT(dateNaissance, '%e %M %Y') as dateNaiss
+        FROM personne p
+        JOIN realisateur r ON r.id_personne = p.id_personne
+        WHERE r.id_realisateur = :id_realisateur
+        ");
+        
+        // Execute la requete pour afficher les info de l'acteur
+        $requeteAfficherActeur->execute([
+            'id_realisateur' => $id
+        ]);
+
+        require "view/detailsRealisateur.php";
+    }
+
+}
