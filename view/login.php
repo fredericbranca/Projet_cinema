8f72e8448e54c3e0c4a2737d9685e2f1cde165d0
Frederic Branca <frederic.branca@gmail.com>
2023-05-30 14:50:25 +0200

    add login view

---
+++ view/login.php
@@ -0,0 +1,24 @@
+<?php
+
+ob_start();
+
+?>
+
+<section id="login">
+    <form method="POST" action="index.php?action=login" enctype="multipart/form-data">
+        <label for="email">Email :</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="password">Mot de passe :</label>
+        <input type="password" id="password" name="password" required>
+
+        <button type="submit" name="login" id="login">Se connecter</button>
+    </form>
+</section>
+
+<?php
+
+$titre = "Connexion";
+$titre_secondaire = "Login";
+$contenu = ob_get_clean();
+require "view/template.php";
