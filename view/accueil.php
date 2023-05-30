460da305dddaafc41f8d1c6aaed73ac4c2794443
Frederic Branca <frederic.branca@gmail.com>
2023-05-30 11:31:42 +0200

    add class for bg and caroussel view accueil

--- view/accueil.php
+++ view/accueil.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 
-ob_start(); 
+ob_start();
 $films = $requeteFilms->fetchAll();
 
 ?>
@@ -17,41 +17,44 @@ $films = $requeteFilms->fetchAll();
         </ul>
         <h1>CNEMA Cretria</h1>
         <p>Site de référence du cinéma !</p>
-        <a href="#">Cinéma à proximité</a>
+        <a href="#listFilms">Voir les films récent</a>
     </div>
 </section>
 
 <section id="listFilms">
-    <div class="header-films">
-        <h2>Films</h2>
-        <div class="whiteline"></div>
-    </div>
-    <div id="container-film">
-        <div class="chevron" id="chevron-gauche"><i class="fa-solid fa-chevron-left"></i></div>
-        <div class="chevron" id="chevron-droit"><i class="fa-solid fa-chevron-right"></i></div>
-        <div id="caroussel">
+    <div class="blur">
+        <div class="header-films">
+            <h2>Films récent</h2>
+            <div class="whiteline"></div>
+        </div>
+        <div id="container-film">
+            <div class="chevron" id="chevron-gauche"><i class="fa-solid fa-chevron-left"></i></div>
+            <div class="chevron" id="chevron-droit"><i class="fa-solid fa-chevron-right"></i></div>
+            <div id="caroussel">
 
-            <div class="cards">
+                <div class="cards">
 
-        <?php
-        foreach($films as $film) { ?>
-            
-                <div class="card-film">
-                    <img src="public/img/<?= $film['affiche'];?>" alt="Affiche du Film : <?= $film['titre'];?>">
-                    <div class="info-film">
-                        <h2><?= $film['titre'];?></h2>
-                        <p>
-                            <?php if($film['duree'] > 59) {
-                                    echo $film['dureeFormat'];
-                                }
-                                else {echo $film['duree'] . " MIN";};?>
-                        </p>
-                        <p>Sortie le <?= $film['dateSortie'];?></p>
-                    </div>
-                    <a href="index.php?action=detailsFilm&id=<?= $film['id_film']?>">Détails du film</a>
-                </div> 
-        <?php } ?>
+                    <?php
+                    foreach ($films as $film) { ?>
 
+                        <div class="card-film">
+                            <img src="public/img/<?= $film['affiche']; ?>" alt="Affiche du Film : <?= $film['titre']; ?>">
+                            <div class="info-film">
+                                <h2><?= $film['titre']; ?></h2>
+                                <p>
+                                    <?php if ($film['duree'] > 59) {
+                                        echo $film['dureeFormat'];
+                                    } else {
+                                        echo $film['duree'] . " MIN";
+                                    }; ?>
+                                </p>
+                                <p>Sortie le <?= $film['dateDeSortie']; ?></p>
+                            </div>
+                            <a href="index.php?action=detailsFilm&id=<?= $film['id_film'] ?>">Détails du film</a>
+                        </div>
+                    <?php } ?>
+
+                </div>
             </div>
         </div>
     </div>
