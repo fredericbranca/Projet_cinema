8ab5a24119c099c3ebc2310f3d43410513baa2f8
Frederic Branca <frederic.branca@gmail.com>
2023-06-01 14:46:48 +0200

    query for listFilms view

--- controller/ListeFilmsController.php
+++ controller/ListeFilmsController.php
@@ -15,8 +15,17 @@ class ListeFilmsController
 
         // Requete pour récupérer les infos de la table film
         $requeteAfficherFilms = $pdo->query("
-            SELECT *
-            FROM film
+            SELECT *, TIME_FORMAT(SEC_TO_TIME(duree * 60), '%k H %i') AS dureeFormat, DATE_FORMAT(dateSortie, '%e %M %Y') as dateDeSortie
+            FROM film f
+            ORDER BY note DESC, titre ASC
+        ");
+
+        // Requete pour récupérer les genres avec les id des films
+            $requeteAfficherFilmsGenres = $pdo->query("
+            SELECT f.id_film, g.id_genre, g.nom
+            FROM film f
+            JOIN genre_film gf ON gf.id_film = f.id_film
+            JOIN genre g ON g.id_genre = gf.id_genre
             ORDER BY note DESC, titre ASC
         ");
 
@@ -24,6 +33,20 @@ class ListeFilmsController
         $requeteAfficherAnnees = $pdo->query("
             SELECT DISTINCT YEAR(dateSortie) as year
             FROM film
+        ");
+
+        // Requete pour récupérer les infos du réalisateur
+        $requeteAfficherRealisateurs = $pdo->query("
+            SELECT id_realisateur, CONCAT(nom, ' ', prenom) as name
+            FROM realisateur r
+            JOIN personne p ON p.id_personne = r.id_personne
+        ");
+
+        // Requete pour récupérer les infos des acteurs
+        $requeteAfficherActeurs = $pdo->query("
+            SELECT id_acteur, CONCAT(nom, ' ', prenom) as name
+            FROM acteur a
+            JOIN personne p ON p.id_personne = a.id_personne
         ");
 
         // Requete pour récupérer les infos de la table genre
