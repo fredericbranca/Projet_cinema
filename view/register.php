c5256ef9dbdc046a28fc218bf567d703d19216d2
Frederic Branca <frederic.branca@gmail.com>
2023-06-02 16:55:43 +0200

    add div class for css

--- view/register.php
+++ view/register.php
@@ -1,6 +1,6 @@
 <?php
 
-if (isset($_SESSION['user'])) { 
+if (isset($_SESSION['user'])) {
     $_SESSION['messageError'] = "Page inaccessible, vous êtes déjà connecté.";
     header("Location: index.php?action=accueil");
     exit;
@@ -10,22 +10,24 @@ ob_start();
 
 ?>
 
-<section id="register">
-    <form method="POST" action="index.php?action=register" enctype="multipart/form-data">
-        <label for="username">Nom d'utilisateur :</label>
-        <input type="text" id="username" name="username" required>
+<section id="register-container">
+    <div id="user">
+        <form method="POST" action="index.php?action=register" enctype="multipart/form-data">
+            <label for="username">Nom d'utilisateur :</label>
+            <input type="text" id="username" name="username" required autocomplete="off">
 
-        <label for="email">Email :</label>
-        <input type="email" id="email" name="email" required>
+            <label for="email">Email :</label>
+            <input type="email" id="email" name="email" required autocomplete="off">
 
-        <label for="password">Mot de passe :</label>
-        <input type="password" id="password" name="password" required>
+            <label for="password">Mot de passe :</label>
+            <input type="password" id="password" name="password" required autocomplete="off">
 
-        <label for="confirm_password">Confirmez le mot de passe :</label>
-        <input type="password" id="confirm_password" name="confirm_password" required>
+            <label for="confirm_password">Confirmez le mot de passe :</label autocomplete="off">
+            <input type="password" id="confirm_password" name="confirm_password" required>
 
-        <button type="submit" name="register" id="register">S'inscrire</button>
-    </form>
+            <button type="submit" name="register" id="register">S'inscrire</button>
+        </form>
+    </div>
 </section>
 
 <?php
